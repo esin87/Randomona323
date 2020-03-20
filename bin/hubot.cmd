91ac58258ad776690f05fd762edb764a5690b453
esin87 <esin.saribudak@gmail.com>
2020-03-20 15:59:53 -0500

    update hubot settings

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "randomona323" %* 
+node_modules\.bin\hubot.cmd --name "Randomona323" %* 
